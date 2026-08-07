b2f1d6934ee699eeb73e07ffaf6831560c3a8e2a
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-08-07 18:51:02 +0530

    Add files via upload

---
+++ IrisCaseStudy2.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_iris
+
+def main():
+    print("-"*30)
+    print("Iris classification case study") 
+    print("-"*30)
+    
+    Dataset = load_iris()  #load iris dataset
+    
+   # Meta Data of the dataset
+    print("Independent variables are :")
+    print(Dataset.feature_names)
+    
+    print("Dependent variables is :")
+    print(Dataset.target_names)
+    
+if __name__ == "__main__":
+    main()
