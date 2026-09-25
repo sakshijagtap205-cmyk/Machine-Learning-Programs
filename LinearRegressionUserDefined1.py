e147215769dbeaba7967c98481566c3adb199f1a
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-09-25 20:29:51 +0530

    Add files via upload

---
+++ LinearRegressionUserDefined1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def MarvellousPredictor():
+    #Load the data
+
+    X = [1,2,3,4,5]
+    Y = [3,4,2,4,5]
+
+    print("Values of independant variable X",X)
+    print("Values of dependant variable Y",Y)
+
+def main():
+    MarvellousPredictor()
+
+if __name__ == "__main__":
+    main()
---
+++ LinearRegressionUserDefined5.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def MarvellousPredictor():
+    #Load the data
+
+    X = [1,2,3,4,5]
+    Y = [3,4,2,4,5]
+
+    print("Values of independant variable X",X)
+    print("Values of dependant variable Y",Y)
+
+    sum_x = 0
+    sum_y = 0
+
+    for i in range(len(X)):
+        sum_x = sum_x + X[i]
+        sum_y = sum_y + Y[i]
+
+    mean_x = sum_x / len(X)
+    mean_y = sum_y / len(Y)   
+
+    print("Mean_X is :",mean_x) 
+    print("Mean_Y is :",mean_y) 
+
+    n = len(X)  #5
+
+    numerator = 0
+    denomerator = 0
+
+    # m =
+    #claculate the slope m
+    for i in range(n):
+        numerator = numerator + ((X[i]-mean_x)*(Y[i]-mean_y))
+        denomerator = denomerator + ((X[i]-mean_x)**2)
+
+    m = numerator / denomerator
+
+    print("Slope of line is m :",m)
+
+    #y = mX + c
+    # c = y-mX
+    # c = ymean - m* xmean
+
+    c = mean_y - m*mean_x
+    print("Y intersect is c :", c)
+def main():
+    MarvellousPredictor()
+
+if __name__ == "__main__":
+    main()
